54171026274abdef3867f66b3b801602defabaa2
Chase Southard <chase.southard@gmail.com>
2019-05-13 09:54:21 -0400

    variably show pill based on status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def active_class(name)
     name.include?(controller_name) ? "active" : controller_name
   end
+
+  def partner_status_badge(partner)
+    if @partner.partner_status == "Verified"
+      content_tag :span, partner.partner_status, class: ["badge", "badge-pill", "badge-primary", "float-right"]
+    elsif @partner.partner_status == "Recertification Required"
+      content_tag :span, partner.partner_status, class: ["badge", "badge-pill", "badge-danger", "float-right"]
+    end
+  end
 end
